4f0fee8ff4c23563f32f44f39e72f2997b97c5af
Ryan Jiang <rui.jiang@pekall.com>
2021-02-07 09:32:44 +0800

    同步消息中添加vmware租户信息

--- datascience/pandas005func02.py
+++ datascience/pandas005func02.py
@@ -32,3 +32,34 @@ s[:'d']
 
 #%%
 s.loc[:'d']
+
+#%%
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.arange(12).reshape(3,4))
+
+#%% dataframe和series之间的操作
+df - df.iloc[0] # df的每一行都减去series
+
+#%%
+df
+
+#%% 矩阵减去一个列向量
+df.sub(df[0], axis=0)
+
+#%% 矩阵减去一个行向量
+arr = np.arange(12).reshape(3,4)
+
+print(arr)
+print(arr[0])
+
+arr - arr[0]
+
+#%% 对所有成员应用函数
+
+df = pd.DataFrame(np.random.randn(4,3))
+df
+
+np.abs(df)
+
--- numpy/akshare_test.py
+++ numpy/akshare_test.py
@@ -23,6 +23,13 @@ for i in range(len(js_news_df)):
     print(f"{js_news_df.iloc[i, 1]}")
     print()
 
+#%% 央行公开操作
+
+# 逆回购：央行给商业银行贷款，商业银行抵押债券给央行。到期后商业银行还钱，收回债券
+
+import akshare as ak
+macro_china_gksccz_df = ak.macro_china_gksccz()
+print(macro_china_gksccz_df)
 
 #%% 银行间拆借利率
 
@@ -34,13 +41,32 @@ rate_interbank_df = ak.rate_interbank(market="上海银行同业拆借市场",
 del rate_interbank_df['涨跌(BP)']
 rate_interbank_df.plot()
 
+#%% 十年期国债
+# "bond_investing_global" # 全球债券行情数据
+
+
+import akshare as ak
+bond_investing_global_df = ak.bond_investing_global(
+    country="中国", index_name="中国10年期国债",
+    period="每周", start_date="2010-01-01", end_date="2021-02-05")
+print(bond_investing_global_df)
+
+#%% 市盈率
+
+import akshare as ak
+stock_a_pe_df = ak.stock_a_pe(market="000016.XSHG")
+print(stock_a_pe_df)
+
+#%%  上海证券交易所-股票数据总貌
+import akshare as ak
+stock_sse_summary_df = ak.stock_sse_summary()
+print(stock_sse_summary_df)
+#%%
 
 #%%
 
 macro_china_lpr_df = ak.macro_china_lpr()
 print(macro_china_lpr_df)
-
-
 
 macro_china_gdp_yearly_df = ak.macro_china_gdp_yearly()
 print(macro_china_gdp_yearly_df)
