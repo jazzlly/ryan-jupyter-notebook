326aecf13ad5fa573bf73bacd2b4d82dd02dc770
Ryan Jiang <rui.jiang@pekall.com>
2021-03-10 09:15:06 +0800

    updated

--- numpy/data_frame_test.py
+++
@@ -1,9 +0,0 @@
-#%%
-
-import pandas_datareader.data as web
-import datetime
-
-#获取上证指数的2017.1.1日至今的交易数据
-df_stockload = web.DataReader("000001.SS", "yahoo", datetime.datetime(2017,1,1), datetime.date.today())
-
-print(df_stockload.head())  # 查看前几行
